a040b3b75f518ecb72db9567933573ea67c03ab3
hung pham <hungpham@teqnological.asia>
2023-06-06 10:11:48 +0700

    temp

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier/skip-formatting",
@@ -13,6 +13,7 @@ module.exports = {
   rules: {
     "prettier/prettier": ["error"],
     "prefer-const": "error",
+    "no-var": "error",
     "no-console": "error",
     "@typescript-eslint/no-unused-vars": "off",
     "unused-imports/no-unused-imports": "error",
